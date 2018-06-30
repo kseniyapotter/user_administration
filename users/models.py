b127ad94728e33149c1dac99fd973ff8388a0b8e
kpotter <kseniya.potter@gmail.com>
2018-06-30 18:50:38 +0200

    Added validation of IBAN field

--- users/models.py
+++ users/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
+from localflavor.generic.models import IBANField
+from localflavor.generic.countries.sepa import IBAN_SEPA_COUNTRIES
 
 class CustomerUser(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
-    iban = models.CharField(max_length=34)
+    iban = IBANField(include_countries=IBAN_SEPA_COUNTRIES)
     user_created = models.ForeignKey(User, on_delete=models.CASCADE)  # admin who created this user
 
     def __str__(self):
